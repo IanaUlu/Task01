ac9ad6aaa6cfc5df64e4d7581a81936c795363bd
IanaUlu <60018329+IanaUlu@users.noreply.github.com>
2021-01-10 23:58:58 +0400

    init

---
+++ isProperly.cs
@@ -0,0 +1,51 @@
+﻿///
+/// Name: isProperly          
+/// Date: 08.01.2021         
+/// Description: Validate brackets in string
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "()(())(())(())(";
+
+            bool answer = isProperly(str);
+
+            if (answer) Console.WriteLine("String is Valid");
+            else Console.WriteLine("String is Not Valid");
+
+        }
+
+        public static bool isProperly(String str)
+        {
+            var stack = new Stack<char>();
+            foreach (var bracket in str)
+            {
+                switch (bracket)
+                {
+                    case '(':
+                        stack.Push(bracket);
+                        break;
+                    case ')':
+                        if ((stack.Count == 0) || (stack.Pop() != '('))
+                            return false;
+                        break;
+                }
+
+            }
+
+            return stack.Count == 0;
+        }
+
+    }
+}
+
