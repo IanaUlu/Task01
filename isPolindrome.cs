ed5179daba56b0bf483945623e53c30dd441356a
IanaUlu <60018329+IanaUlu@users.noreply.github.com>
2021-01-11 00:13:12 +0400

    init

---
+++ isPolindrome.cs
@@ -0,0 +1,31 @@
+﻿///
+/// Name: isPolindrome          
+/// Date: 08.01.2021         
+/// Description: Function returns input string is Polindrome or not
+///
+
+static string isPolindrome(String str)
+        {
+            string checkStr;
+
+            str = str.ToLower();
+
+            if (!string.IsNullOrEmpty(str))
+            {
+                // Reverse order elements of array
+                checkStr = new string(str.Reverse().ToArray());
+                if (str == checkStr)
+                {
+                    return str + " is Polindrome";
+                }
+                else
+                {
+                    return str + " is not Polindrome";
+                }
+            }
+           return "Wrong value";
+        }
+
+
+
+
